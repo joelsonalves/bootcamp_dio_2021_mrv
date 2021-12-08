c422b6c81c1c0ec79320647a5c91f5ded5c76049
Joelson Alves <joelsonalves@yahoo.com.br>
2021-12-08 19:11:37 -0200

    atualização

--- introducao_js/js/scripts.js
+++ introducao_js/js/scripts.js
@@ -1,1 +1,17 @@
-console.log("Hello World!")
+var buttonMenos = document.querySelectorAll('section.contador button')[0];
+var buttonMais = document.querySelectorAll('section.contador button')[1]
+var contador = document.querySelector('section.contador span')
+
+console.log("Hello World!");
+
+buttonMenos.addEventListener('click', () => {
+    if (contador.textContent > 0) {
+        contador.textContent = (parseInt(contador.textContent) - 1).toString();
+    }
+});
+
+buttonMais.addEventListener('click', () => {
+    if (contador.textContent < 10) {
+        contador.textContent = (parseInt(contador.textContent) + 1).toString();
+    }
+});
