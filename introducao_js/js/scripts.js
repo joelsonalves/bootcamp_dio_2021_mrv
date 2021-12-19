128985b26553c8085fbb55bddd96bf2e2d6da619
Joelson Alves <joelsonalves@yahoo.com.br>
2021-12-18 21:40:41 -0300

    Uso da CAT API

--- introducao_js/js/scripts.js
+++ introducao_js/js/scripts.js
@@ -1,8 +1,9 @@
-var buttonMenos = document.querySelectorAll('section.contador button')[0];
-var buttonMais = document.querySelectorAll('section.contador button')[1]
-var contador = document.querySelector('section.contador span')
-
 console.log("Hello World!");
+
+
+var buttonMenos = document.querySelectorAll('section.contador button')[0];
+var buttonMais = document.querySelectorAll('section.contador button')[1];
+var contador = document.querySelector('section.contador span');
 
 buttonMenos.addEventListener('click', () => {
     if (contador.textContent > 0) {
@@ -15,3 +16,24 @@ buttonMais.addEventListener('click', () => {
         contador.textContent = (parseInt(contador.textContent) + 1).toString();
     }
 });
+
+
+const URL_CAT_API = 'https://thatcopy.pw/catapi/rest/';
+const buttonTrocarGato = document.querySelector('section.imagem-de-gato button');
+const imagemGato = document.querySelector('section.imagem-de-gato img');
+
+const getImagemGato = async() => {
+    const data = await fetch(URL_CAT_API)
+    .then(resultado => resultado.json())
+    .catch(e => console.log(e.message));
+
+    return data.webpurl;
+};
+
+const trocarImagem = async() => {
+    imagemGato.src = await getImagemGato(); 
+};
+
+buttonTrocarGato.addEventListener('click', trocarImagem);
+
+trocarImagem();
